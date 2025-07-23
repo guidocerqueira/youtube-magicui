b247286b6b9b0a7c1767d9a95d683b8489684abe
Guido Cerqueira <euguidocerqueira@gmail.com>
2025-07-23 19:11:42 -0300

    implement magicui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { BentoDemo } from "@/components/bento-grid";
+import { NeonGradientCardDemo } from "@/components/card-neon";
+import { LensDemo } from "@/components/lens";
+import { Meteor } from "@/components/meteors";
+
 export default function Home() {
-  return <h1>Hello, World!</h1>
+  return (
+    <div className="flex flex-col gap-4 px-48 py-8">
+      <NeonGradientCardDemo />
+      <Meteor />
+      <BentoDemo />
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <LensDemo />
+        <LensDemo />
+        <LensDemo />
+      </div>
+    </div>
+  );
 }
