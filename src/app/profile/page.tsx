403abda14838874938b2cb8cdf27e10b2ff5efa4
Guido Cerqueira <euguidocerqueira@gmail.com>
2025-07-25 18:16:48 -0300

    implement originui

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import ProfileCropImageUpload from "@/components/crop-image";
+
+export default function Page() {
+	return (
+		<div className="flex h-screen w-full items-center justify-center">
+			<ProfileCropImageUpload />
+		</div>
+	)
+}
